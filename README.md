b089d97b5d0b61c767d334545d0b5a0bed6f6844
Glitch (ty-metricimpconverter-python) <none>
2020-06-22 23:21:28 +0000

    Import from Glitch

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# metricimpconverter-python
-Python port of my Node.js metric-imperial converter microservice project.
+# Python file metadata microservice
+
+This is a Python/[Flask](https://flask.palletsprojects.com/en/1.1.x/) port of my [Node.js file metadata microservice project](https://ty-file-metadata.glitch.me/), which was originally built to fulfill the following user stories:
+
+1.  I can submit a form object that includes a file upload.
+2.  The from file input field has the `name` attribute set to `"upfile"`. We rely on this in testing.
+3.  When I submit something, I will receive the file name and size in bytes within the JSON response.
---
+++ server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timezone
+from flask import Flask, request, render_template, jsonify
+import convert
+
+app = Flask(__name__, static_folder="public", template_folder="views")
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+@app.route("/api/convert")
+def metric_imp_converter():
+    input = request.args["input"].replace(" ", "")
+    init_num = convert.get_num(input)
+    init_unit = convert.get_unit(input)
+    return_num = convert.convert(init_num, init_unit)
+    return_unit = convert.get_return_unit(init_unit)
+    string = convert.get_string(init_num, init_unit, return_num, return_unit)
+
+    return jsonify(
+        initNum=init_num,
+        initUnit=init_unit,
+        returnNum=return_num,
+        returnUnit=return_unit,
+        string=string,
+    )
+
+
+if __name__ == "__main__":
+    app.run()
